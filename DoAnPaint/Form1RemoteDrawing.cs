2f546eb27f0afc4999e4b313b369e103597bab18
Nguyễn Viết Tùng <nchinhtung@gmail.com>
2024-12-07 13:20:59 +0700

    abcxyz

--- DoAnPaint/Form1RemoteDrawing.cs
+++ DoAnPaint/Form1RemoteDrawing.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DoAnPaint
 {
-    public partial class Form1
+    public partial class Form1 : Form
     {
         private void HandleData()
         {
