09eb5a0e67d080f2292e7f08ba228fa80fa331a7
Nguyễn Viết Tùng <nchinhtung@gmail.com>
2024-12-16 00:08:13 +0700

    Add het tinh nang cua Thanh

--- CoreApp/forgetpw.cs
+++ CoreApp/forgetpw.cs
@@ -1,28 +1,92 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Dreaw
 {
     public partial class forgetpw : Form
     {
+        bool isSending;
+        const string serverAdd = "https://localhost:7183";
         public forgetpw()
         {
             InitializeComponent();
         }
 
-        private void pictureBox3_Click(object sender, EventArgs e)
+        private async void pictureBox3_Click(object sender, EventArgs e)
         {
-            // Khởi tạo và mở Form2
-            code newForm = new code("1234", "a", "b", "c");
-            newForm.Show();
-            this.Close();
+            if (isSending)
+            {
+                MessageBox.Show("Completed the last request first!");
+                return;
+            }
+            string email = txtEmail.Text.Trim();
+            if (string.IsNullOrEmpty(email))
+            {
+                MessageBox.Show("Please fill email box!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!IsValidEmail(email))
+            {
+                MessageBox.Show("Invalid email format!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            // Tạo dữ liệu cần gửi
+            var requestData = new
+            {
+                Email = email
+            };
+            using (var client = new HttpClient())
+            {
+                isSending = true;
+                var jsonRequest = JsonConvert.SerializeObject(requestData);
+                var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+                Cursor.Current = Cursors.WaitCursor;
+                var response = await client.PostAsync($"{serverAdd}/api/forgetpw", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var responseObject = JsonConvert.DeserializeObject<dynamic>(responseContent);
+                    string otp = responseObject?.otp;
+                    code newForm = new code(otp, email, "forgetpw");
+                    isSending = false;
+                    Cursor.Current = Cursors.Default;
+                    MessageBox.Show("A new OTP has been sent to your email.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    newForm.Show();
+                    this.Close();
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.Conflict)
+                {
+                    MessageBox.Show("Email not exists! Please sign up!");
+                    Cursor.Current = Cursors.Default;
+                    isSending = false;
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("An error has occured!");
+                    Cursor.Current = Cursors.Default;
+                    isSending = false;
+                    return;
+                }
+            }
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            // Sử dụng Regex để kiểm tra định dạng email
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, emailPattern);
         }
     }
 }
